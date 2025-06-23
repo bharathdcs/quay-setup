f8651220b151f656e8522d0ca2060ae9be66ea8f
BHARATH KUMAR DEVARAJU <bkdevara@au1.ibm.com>
2025-06-23 18:33:50 +1000

    updated install script

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ mkdir $HOME/quay-installer
 cd $HOME/quay-installer
 wget https://github.com/quay/mirror-registry/releases/download/v2.0.6/mirror-registry-online.tar.gz
 tar -xzvf mirror-registry-online.tar.gz
-./mirror-registry install
+$HOME/quay-installer/mirror-registry install
